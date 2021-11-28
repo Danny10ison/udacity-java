3b7fe3855e4a39f7bd762a7bb17bdc4d0c43fbf8
Danny10ison <pagesngod@gmail.com>
2021-11-28 22:39:36 +0000

    Simply Calculator App

---
+++ src/phase_one/lesson_5/scanner/SimplyCalculator.java
@@ -0,0 +1,30 @@
+package phase_one.lesson_5.scanner;
+
+import java.util.Scanner;
+
+public class SimplyCalculator {
+
+    public static int sumNumbers(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Simple CLI calculator");
+        System.out.println("--------------------------\n");
+        try{
+            System.out.println("Enter first number");
+            int num1 = scanner.nextInt();
+            System.out.println("Enter second number");
+            int num2 = scanner.nextInt();
+
+            System.out.println( num1 + " + " + num2 + " = " + sumNumbers(num1,num2));
+
+        } catch (Exception ex){
+            ex.getLocalizedMessage();
+        } finally {
+            System.out.println("Wrong input");
+            scanner.close();
+        }
+    }
+}
